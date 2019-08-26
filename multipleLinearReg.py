a9201cc7276ffd15756b163c997b674a9c6fbe43
Bijay2305 <52318917+Bijay2305@users.noreply.github.com>
2019-08-27 01:53:20 +0530

    multiple linear reg created

---
+++ multipleLinearReg.py
@@ -0,0 +1,3 @@
+Multiple Linear Regression
+Now you know how to build a model with one X (feature variable) and Y (response variable). But what if you have three feature variables, or may be 10 or 100? Building a separate model for each of them, combining them, and then understanding them will be a very difficult and next to impossible task. By using multiple linear regression, you can build models between a response variable and many feature variables.
+Let's see how to do that.
